0fa42f1fc4e298bb504cf44236bab5f0d86c9ec9
Yashraj Maheshwari <yashrajmaheshwari15@gmail.com>
2022-02-11 23:54:28 +0530

    file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
+
+/*
+For custom theme refer bottom link
+https://getstream.io/chat/docs/sdk/react/customization/css_and_theming/#css-variables
+*/
